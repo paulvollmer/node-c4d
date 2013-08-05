dc100d91bc391f62576daee67eccb23dacd19a9d
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-08-05 21:28:36 +0200

    bin/c4d-cli.js added more commander options

--- bin/c4d-cli.js
+++ bin/c4d-cli.js
@@ -7,7 +7,6 @@
  * @author: Paul Vollmer
  */
 
-
 /**
  * Module dependencies.
  */
@@ -22,7 +21,13 @@ var c4d = require('../src/index.js');
  */
 program
   .version(pkg.version)
-  .option('-r, --render [filepath]', 'Render the added file.')
+  .option('-r, --render [filepath]', 'specify a file to render')
+  .option('-f, --frame [from] [to] [step]', 'specify start frame, end frame and frame step. "to" and "step" are optional')
+  .option('-i, --oimage [imagename]', 'override the image output path for rendering')
+  .option('-m, --omultipass [imagename]', 'override the multipass output path for rendering')
+  .option('-f, --oformat [imageformat]', 'override the image output format to TIFF/TGA/BMP/IFF/JPG/PICT/PSD/PSB/RLA/RPF/B3D')
+  .option('-e, --oresolution [width] [height]', 'override output image size')
+  .option('-t, --threads [threadcnt]', 'specify number of threads (0 for auto-detection)')
   .option('-s, --silent', 'silent mode. don\'t output anything')
 program.on('--help', function() {
   console.log('  Description:');
@@ -36,7 +41,6 @@ program.on('--help', function() {
   console.log('');
 });
 program.parse(process.argv);
-
 
 /**
  * If no option are set. print out the help.
