59520b407758de96cf361ac0e9bdc090cc751bd8
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-08-06 17:06:23 +0200

    bin/c4d-cli.js added c4d.render parameter, changes at frame, resolution options

--- bin/c4d-cli.js
+++ bin/c4d-cli.js
@@ -22,11 +22,11 @@ var c4d = require('../src/index.js');
 program
   .version(pkg.version)
   .option('-r, --render [filepath]', 'specify a file to render')
-  .option('-f, --frame [from] [to] [step]', 'specify start frame, end frame and frame step. "to" and "step" are optional')
+  .option('-f, --frame [from,to,step]', 'specify start frame, end frame and frame step. "to" and "step" are optional')
   .option('-i, --oimage [imagename]', 'override the image output path for rendering')
   .option('-m, --omultipass [imagename]', 'override the multipass output path for rendering')
   .option('-f, --oformat [imageformat]', 'override the image output format to TIFF/TGA/BMP/IFF/JPG/PICT/PSD/PSB/RLA/RPF/B3D')
-  .option('-e, --oresolution [width] [height]', 'override output image size')
+  .option('-e, --oresolution [width,height]', 'override output image size')
   .option('-t, --threads [threadcnt]', 'specify number of threads (0 for auto-detection)')
   .option('-s, --silent', 'silent mode. don\'t output anything')
 program.on('--help', function() {
@@ -54,9 +54,14 @@ if (program.render == undefined) {
 else {
   if (program.render) {
     c4d.render({
-      silent: program.silent,
       filepath: program.render,
-      out_format: 'TIFF'
+      frame: program.frame,
+      oimage: program.oimage,
+      omultipass: program.omultipass,
+      oformat: program.oformat,
+      oresolution: program.oresolution,
+      threads: program.threads,
+      silent: program.silent
     });
   }
 }
