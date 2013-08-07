9f8b76beb46b6e1ce9f293ee770f64f067e73c11
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-08-07 13:26:13 +0200

    src/report.js WIP

--- src/report.js
+++ src/report.js
@@ -2,24 +2,34 @@
  * Module dependencies.
  */
 var fs = require('fs');
+var utils = require('./utils.js');
+
+/**
+ * Filename of the report file.
+ */
+var filename = 'report.txt';
 
 /**
  * Write a report text file.
  *
- * @param {String} data
  * @param {String} filepath
+ * @param {String} data
+ * @param {Boolean} silent
  */
-exports.write = function(data, filepath) {
+exports.write = function(filepath, data, silent) {
   /*
    * If no filepath is set...
    */
   if (filepath === true) {
-    fs.writeFile('report.txt', data);
+    fs.writeFile(filename, data);
+    utils.log(silent, 'Report saved to current working directory');
   }
   /*
    * If a filepath is set...
    */
   else {
-    fs.writeFile(filepath+'/report.txt', data);
+  	var tmpPath = filepath+'/'+filename;
+    fs.writeFile(tmpPath, data);
+    utils.log(silent, 'Report saved to \''+tmpPath+'\'');
   }
 }
