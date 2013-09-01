5866d1a4424e740149317de8ab3fc12d13a4eb8c
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-01 12:58:19 +0200

    update test.cinema4d_options_general and _render.js

--- test/test.cinema4d_options_general.js
+++ test/test.cinema4d_options_general.js
@@ -2,9 +2,9 @@ var assert = require('assert');
 var c4d_options_general = require('./../src/cinema4d_options_general.js');
 
 suite('cinema4d_options_general.js', function() {
-  suite('nogui()', function() {
-    test('Without data.', function() {
-      assert.equal( 'test', c4d_options_general.nogui.get() );
+  suite('nogui', function() {
+    test('getOptionsArray()', function() {
+      assert.deepEqual( ['-nogui'], c4d_options_general.nogui.getOptionsArray() );
     });
   });
 });
